7612c84e5b9bf5f6301b6308b2b6383c83c2b447
Salomão Junio Santos Batista <salomaojunio@hotmail.com>
2021-07-22 14:48:09 -0300

    sortedBy

--- src/main/kotlin/br/com/salosoftwares/apicollectionsok/TestListOf.kt
+++ src/main/kotlin/br/com/salosoftwares/apicollectionsok/TestListOf.kt
@@ -11,6 +11,8 @@ fun main() {
     println(LINE)
     println("lulu's information")
     println(employees.find { it.name=="lulu" })
+    println(LINE)
+    employees.sortedBy { it.salary }.forEach { println(it) }
 }
 data class Employee(val name:String,val salary:Double){
     override fun toString(): String="""
