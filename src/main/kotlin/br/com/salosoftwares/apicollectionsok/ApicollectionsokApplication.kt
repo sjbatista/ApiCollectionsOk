eead974bb95de58ee9b7d1cc6e5d1552ea62d1fc
Salomão Junio Santos Batista <salomaojunio@hotmail.com>
2021-07-22 10:15:17 -0300

    Training with Arrays

--- src/main/kotlin/br/com/salosoftwares/apicollectionsok/ApicollectionsokApplication.kt
+++ src/main/kotlin/br/com/salosoftwares/apicollectionsok/ApicollectionsokApplication.kt
@@ -4,9 +4,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ApicollectionsokApplication
 
+class ApicollectionsokApplication
+const val LINE="-------------------"
 fun main(args: Array<String>) {
 	runApplication<ApicollectionsokApplication>(*args)
-	println("teste")
+	println(LINE)
+	println("intArray1:")
+	val intArray1 = IntArray(5)
+	intArray1[0]=5
+	intArray1[1]=4
+	intArray1[2]=2
+	intArray1[3]=1
+	intArray1[4]=3
+	intArray1.forEach {println(it)}
+	println("Sorted intArray1:")
+	intArray1.sort()
+	intArray1.forEach {println(it)}
+	println(LINE)
+	val intArray2 = intArrayOf(30,10,50,20,40) //This is an "intArrayOf", where to declare the values immediately
+	println("intArray2:")
+	intArray2.forEach {println(it)}
+	intArray2.sort()
+	println("Sorted intArray2:")
+	intArray2.forEach {println(it)}
+	println(LINE)
+	println("Safe iteration of intArray2, via Index:")
+	for(index in intArray2.indices){ //Safe iteration, via Index
+		println(intArray2[index])
+	}
+
+
 }
